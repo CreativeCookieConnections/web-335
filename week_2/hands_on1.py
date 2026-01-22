66816da297c2f78499c9847eb08c0bf768e8dfca
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-01-22 11:45:18 -0600

    worked on assignment for week 2. First  Python Code

---
+++ week_2/hands_on1.py
@@ -0,0 +1,9 @@
+""" 
+    Author: Aisha Keller
+    Date: January 22, 2026
+    File Name: hands_on1.py
+    Description: This file demonstrates the use of both file header comments and normal level comments
+"""
+
+print("You need to believe in yourself, and unleash that power! -Son Goku aka Kakarot")
+# This line prints an inspirational quote from the character Son Goku in the Dragon Ball series.
